ceeb19bedafd08efb7adf587cdcd7685d5e005ed
jagermi3ster <jagermi3ster@users.noreply.github.com>
2023-02-06 04:57:24 +0000

    deploy: 853180a45e45d74c77f308397e88176325d66fcc

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/jagermi3ster.github.io/assets/js/search-data.json', true);
+    request.open('GET', '/The-Pentagon/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
